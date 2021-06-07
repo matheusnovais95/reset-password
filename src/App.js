05418f8e921a5da2beab33212887c0f02241f070
matheusnovais95 <mtshcampos@gmail.com>
2021-06-07 16:57:08 -0300

    versao 1.0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
+import React from 'react';
+import { useForm } from "react-hook-form";
 import './App.css';
 
 function App() {
+
+  const {
+    register,
+    formState: { errors },
+    getValues,
+    handleSubmit
+  } = useForm();
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container-form">
+        <div className="background-top" />
+        <form onSubmit={handleSubmit((data) => console.log(data))}>
+          <div className="container-form-title">
+            <h2>Redefina sua senha</h2>
+            <p>
+              Por favor, digite sua nova senha abaixo e depois a confirme. Lembrando, elas devem ser iguais.
+            </p>
+          </div>
+          <div className="container-form-input">
+
+            <input
+              type="password"
+              name="password"
+              className="input-password-form"
+              placeholder="Digite sua senha"
+              {...register("password", { required: "Campo obrigatório!" })}
+            />
+            {errors.password && (
+              <span id="msg-error"> {errors.password.message}</span>
+            )}
+            <input
+              type="password"
+              name="passwordConfirm"
+              className="input-password-form"
+              placeholder="Confirme sua senha"
+              {...register("passwordConfirm", {
+                required: "Campo obrigatório!",
+                validate: {
+                  matchesPreviousPassword: (value) => {
+                    const { password } = getValues();
+                    return password === value || "As senhas devem ser iguais!";
+                  }
+                }
+              })}
+            />
+            {errors.passwordConfirm && (
+              <span id="msg-error"> {errors.passwordConfirm.message}</span>
+            )}
+
+            <button type="submit" className="btn-confirm">enviar</button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
